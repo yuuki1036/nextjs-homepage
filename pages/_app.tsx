e29365d91dc121c54b705236434a63058396893b
yuuki1036 <yuuki1036@gmail.com>
2023-01-02 19:31:58 +0900

    init template

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import 'styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'next-themes';
+import { Inter } from '@next/font/google';
+import { Analytics } from '@vercel/analytics/react';
+
+const interVariable = Inter();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider attribute="class">
+      <main className={interVariable.className}>
+        <Component {...pageProps} />
+        <Analytics />
+      </main>
+    </ThemeProvider>
+  );
 }
